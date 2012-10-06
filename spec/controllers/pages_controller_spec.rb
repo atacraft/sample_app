3ce788e466dcc012fe035b29cafe7ec6abd08de8
atacraft <atacraft@gmail.com>
2012-10-06 09:23:56 +0000

    Fin pages statics

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,12 +7,37 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+
+    it "should have the rigth title" do
+      get 'home'
+      response.should have_selector("title",
+        :content => "Simple App | Accueil")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the suitable title" do
+      get 'contact'
+      response.should have_selector("title",
+        :content => "Simple App | Contact")
+    end
+  end
+
+  describe "Get 'About'" do
+    it "should be ok" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the supposed title" do
+      get 'contact'
+      response.should have_selector("title",
+        :content => "Simple App | A Propos")
     end
   end
 
